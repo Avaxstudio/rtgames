19e8a29fb833fbca94a0b78e4df33a5d6bc0f7a4
Aleksandar Vlajkovic <avaxstudio@gmail.com>
2025-02-17 00:16:14 +0100

    first commit

---
+++ provider.tf
@@ -0,0 +1,17 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = "4e18481d-a9da-45f3-8b4b-a98bfa532390"
+}
